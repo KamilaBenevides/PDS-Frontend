cefee9a340e47bad9f4877d9b3098727d96c0a0b
João Falcão <jvfsl@ic.ufal.br>
2022-05-20 16:21:19 -0300

    add alert type selection to defense and quali pages

--- pds-frontend/src/pages/Alerts/Qualification/Qualification.jsx
+++ pds-frontend/src/pages/Alerts/Qualification/Qualification.jsx
@@ -1,8 +1,23 @@
 import BaseAlert from "../BaseAlert";
+import { useState } from 'react';
+import { Radio, Row } from 'antd';
 
 const QualificationAlerts = () => {
+    
+    const [selected, setSelected] = useState("AGENDAMENTO_QUALIFICACAO");
+
+    const handleChange = (e) => {
+        setSelected(e.target.value);
+    }
+
     return <>
-        <BaseAlert alertType="QUALIFICACAO" />
+        <Row justify="center">
+            <Radio.Group value={selected} onChange={handleChange} style={{ marginTop: 8, marginBottom: -10 }}>
+                <Radio.Button value="AGENDAMENTO_QUALIFICACAO">AGENDAMENTO</Radio.Button>
+                <Radio.Button value="QUALIFICACAO">QUALIFICAÇÃO</Radio.Button>
+            </Radio.Group>
+        </Row>
+        <BaseAlert alertType={selected} />
     </>
 }
 
