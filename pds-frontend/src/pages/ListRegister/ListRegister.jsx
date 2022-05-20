f7ace8babf9ae8300bda64e9e46acbf585f5c139
João Falcão <jvfsl@ic.ufal.br>
2022-05-20 17:00:45 -0300

    edit button

--- pds-frontend/src/pages/ListRegister/ListRegister.jsx
+++ pds-frontend/src/pages/ListRegister/ListRegister.jsx
@@ -4,8 +4,9 @@ import client from '../../api/apollo';
 import { Container, StyledNameText, StyledText, StyledButton, StyledContent } from './styles';
 import { useEffect, useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
-import {Col, Row, Typography } from 'antd';
+import {Col, Row, Typography, Button } from 'antd';
 import moment from 'moment';
+import { useNavigate } from "react-router-dom";
 
 const {Text} = Typography;
 
@@ -14,6 +15,7 @@ const ListRegister = () => {
     const queryAlunos = useQuery(gql`
     query GetAlunos {
         alunos {
+            id
             nomeCompleto
             matricula
             dataIngresso
@@ -34,8 +36,9 @@ const ListRegister = () => {
   `);
 
   const queryDocentes = useQuery(gql`
-    query GetAlunos {
+    query GetDocentes {
         docentes {
+            id
             nomeCompleto
             email
           }
@@ -85,6 +88,35 @@ const ListRegister = () => {
 
 const dataFormater = date => moment(date).format("DD/MM/YYYY");
 
+const navigate = useNavigate();
+
+const docContent = item => (
+  <StyledContent>
+    <Row gutter={16}>
+      <Col span={24}>
+        <StyledText><strong>E-mail:</strong> {item.email}</StyledText>
+      </Col>
+      <Col span={3}>
+        <StyledButton onClick={() => handleClickDoc(item)}
+          style={{
+            background: '#838EA0',
+              color: '#FFFFFF'
+            }}>
+          EDITAR
+        </StyledButton>
+      </Col>
+    </Row>
+  </StyledContent>
+)
+
+const handleClick = (item) => {
+  navigate('/dashboard/register/' + item.id);
+}
+
+const handleClickDoc = (item) => {
+  navigate('/dashboard/professor/' + item.id);
+}
+
 const content = item =>
   (<StyledContent>
       <Row gutter={16}>
@@ -114,6 +146,15 @@ const content = item =>
               INATIVAR
             </StyledButton>
           </Col> : null}
+          <Col span={3}>
+            <StyledButton onClick={() => handleClick(item)}
+              style={{
+                background: '#838EA0',
+                  color: '#FFFFFF'
+                }}>
+              EDITAR
+            </StyledButton>
+          </Col>
       </Row>
   </StyledContent>)
     
@@ -121,9 +162,9 @@ const content = item =>
         <Container>
             {header}
             <h4>Discentes</h4>
-            <Collapse items={items} header={discentesHeader} content={content}/>
+            <Collapse items={items} header={discentesHeader} content={content} />
             <h4>Docentes</h4>
-            <Collapse items={itemsDoc} header={docentesHeader}/>
+            <Collapse items={itemsDoc} header={docentesHeader} content={docContent} />
         </Container>
     </> 
 }
