6ca304083dab2e93b9430bfc8c986c99c668f12b
Julios <jslrocha@gmail.com>
2022-06-30 23:59:35 -0300

    adding new backend url and token authentication

--- pds-frontend/src/api/apollo.js
+++ pds-frontend/src/api/apollo.js
@@ -1,12 +1,32 @@
-import { ApolloClient, gql, InMemoryCache } from "@apollo/client"
+import { ApolloClient,
+   gql, 
+   InMemoryCache, 
+   createHttpLink } from "@apollo/client"
+import { setContext } from '@apollo/client/link/context';
 
 export const TOKEN_KEY = "__ppgi_token__";
 
 export const getAuthenticationToken = () => localStorage.getItem(TOKEN_KEY);
 
+const link = createHttpLink({
+  uri: "https://back-pds-ppgi-account.herokuapp.com/",
+  // credentials: 'same-origin'
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = getAuthenticationToken();
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? token : "",
+    }
+  }
+});
+
 export const client = new ApolloClient({
-  uri: "https://back-pds.herokuapp.com/",
-  // uri: "https://pds-apollo.herokuapp.com/",
+  link: authLink.concat(link),
   cache: new InMemoryCache()
 })
 
